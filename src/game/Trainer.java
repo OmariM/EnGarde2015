380670ec015ed6f8b5b323a0bb5dc684271c6b6e
OmariM0017 <you@example.com>
2015-11-17 15:01:11 -0500

    update to the trainer. more complete

--- src/game/Trainer.java
+++ src/game/Trainer.java
@@ -1,6 +1,6 @@
 package game;
 
-import players.Player;
+import players.*;
 
 import java.util.ArrayList;
 
@@ -24,18 +24,30 @@ public class Trainer {
 
 	public static void main(String[] args) {
 		while(true == true){
-			Tournament t = new Tournament();
-			//make the tournament
-			//add the people
-			ArrayList<Player> players = t.playTournament(10000, false);
-			Player winner = players.get(players.size()-1);
-			//winnerBalls = winner.getBalls;
-			//System.out.println(winnerBalls)
-			System.out.println(balls);
-			//Player father = new Player(father, winnerBalls);
-			
-
+			train(0.05);
 		}
+	}
+	
+	public static void train(double balls){
+		int iterations = 0;
+		Tournament t = new Tournament(); 								   //create the tournament
+		t.addPlayer(new blank_space("father", balls)); 					   //add the father with best balls
+		t.addPlayer(new blank_space("v1", balls + 1+(-2)*(Math.random()))); //add the variants
+		t.addPlayer(new blank_space("v2", balls + 1+(-2)*(Math.random()))); //add the variants
+		t.addPlayer(new blank_space("v3", balls + 1+(-2)*(Math.random()))); //add the variants
+		t.addPlayer(new blank_space("v4", balls + 1+(-2)*(Math.random()))); //add the variants
+		t.addPlayer(new blank_space("v5", balls + 1+(-2)*(Math.random()))); //add the variants
+		t.addPlayer(new blank_space("v6", balls + 1+(-2)*(Math.random()))); //add the variants
+		t.addPlayer(new blank_space("v7", balls + 1+(-2)*(Math.random()))); //add the variants
+		t.addPlayer(new blank_space("v8", balls + 1+(-2)*(Math.random()))); //add the variants
+		ArrayList<Player> players = t.playTournament(10000, false);        //play the tournament
+		Player winner = players.get(players.size()-1);					   //get the winner
+		double winnerBalls = ((blank_space)winner).getBalls();			   //get the balls of the winner
+		iterations++;
+		System.out.println("Winner Balls: " + winnerBalls + " || Number of iterations: " + iterations);
+		balls = winnerBalls;											   //set the new balls to best balls
+		train(balls);
+		
 	}
 
 }
